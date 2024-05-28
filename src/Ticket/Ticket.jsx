6c320758a838087bbc57a75b4879ecd0ec1ca29a
Eliecer Tomas Collarte Acevedo <eliecercollarte.ext@tanner.cl>
2024-05-28 18:47:10 -0400

    fix ticket page

--- src/Ticket/Ticket.jsx
+++ src/Ticket/Ticket.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-
+import Nav from "../Top-Nav/Nav";
+import BottomNav from "../bottom-nav/BottomNav";
 const Ticket = ()=>{
     return (
         <div>
+            <Nav/>
             <h1>Ticket</h1>
+            <BottomNav/>
         </div>
     );
 }
