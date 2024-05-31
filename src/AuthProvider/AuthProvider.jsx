9bb7d2ef3d37197b56f0fbd719853a7a8afbc83e
davide.g <davide.garrido@wisely.cl>
2024-05-30 22:24:34 -0400

    test login endpoint

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -7,10 +7,10 @@ export const AuthProvider = ({ children }) => {
 
     const login = async (username, password) => {
         try {
-            /*const response = await axios.post('/api/login', { username, password });
+            const response = await axios.post('https://api-grupo-3.azurewebsites.net/api/v1/auth/login', { username, password });
             if (response.status === 200) {
               setIsAuthenticated(true);
-            }*/
+            }
             setIsAuthenticated(true);
         } catch (error) {
             console.error('Login failed:', error);
